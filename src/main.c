4f666a6359ea2b7fdaab66f3847f57f4d34f5f9c
jlouhela <louhela.joni@gmail.com>
2021-05-30 12:07:45 +0300

    Add filepath option

--- src/main.c
+++ src/main.c
@@ -16,13 +16,15 @@ void print_usage();
 
 int main(int argc, char *argv[])
 {
-    todo t;
+    char file_path[512] = "todomo.bin";
+    enum op operation = op_list;
     int c = -1;
     while (1)
     {
         static struct option long_options[] =
             {
                 {"add", required_argument, 0, 'a'},
+                {"file", required_argument, 0, 'f'},
                 {"list", optional_argument, 0, 'l'},
                 {"help", no_argument, 0, 'h'},
                 {0, 0, 0, 0}};
@@ -30,7 +32,7 @@ int main(int argc, char *argv[])
         // getopt_long stores the option index here.
         int option_index = 0;
 
-        c = getopt_long(argc, argv, ":a:hl::",
+        c = getopt_long(argc, argv, ":a:f:hl::",
                         long_options, &option_index);
 
         // Detect the end of the options.
@@ -60,11 +62,18 @@ int main(int argc, char *argv[])
             break;
 
         case 'a':
+            // TODO handle later, store op
             printf("option -a with value `%s'\n", optarg);
             char desc[TODO_LEN];
             strncpy(desc, optarg, TODO_LEN);
             const todo t = create_todo(desc);
             printf("created todo with desc `%s'\n", t.text);
+            break;
+
+        case 'f':
+            // TODO logger + log levels
+            printf("Set filepath to %s\n", optarg);
+            strcpy(file_path, optarg);
             break;
 
         case 'l':
@@ -96,5 +105,6 @@ void print_usage()
     printf("todomo usage: todomo -args"
            "\n-h: help"
            "\n-a <todo text>: add todo, max 255 characters"
+           "\n-f <file path>: file path (input/output) depending on the context"
            "\n-l <count>: list todos\n");
 }
